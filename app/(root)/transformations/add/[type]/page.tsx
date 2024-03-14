dfde6e8b1a5726e4a2a43ece390f0fbf563f824d
unknown <lounisbousbaine@gmail.com>
2024-03-14 08:46:18 +0100

    implement webhooks clerk

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,14 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
+import { transformationTypes } from '@/constants'
 
-const AddTransformType = () => {
+const AddTransformType = ({params:{type} }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
   return (
-    <div>AddTransformType</div>
-  )
+   <Header 
+   title={transformation.title} 
+   subtitle={transformation.subTitle} />
+    )
 }
 
 export default AddTransformType
