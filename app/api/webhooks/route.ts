d4f3aaeb94f5e8314e2c90326cd251f826b9baed
unknown <lounisbousbaine@gmail.com>
2024-03-15 12:13:40 +0100

    all

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,10 +1,9 @@
 /* eslint-disable camelcase */
 import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-import { Webhook } from "svix";
-
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
 export async function POST(req: Request) {
